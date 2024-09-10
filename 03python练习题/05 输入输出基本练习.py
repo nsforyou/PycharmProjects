a0b3067f2d672a82cfd2cb1180970ff912054d26
Jhon Price <36687664+nsforyou@users.noreply.github.com>
2024-09-11 04:40:20 +0800

    测试变量输入与字符串拼接

--- 03python练习题/05 输入输出基本练习.py
+++ 03python练习题/05 输入输出基本练习.py
@@ -1,3 +1,12 @@
+'''
 username = input('username: ')
 print('welcome', username)   # print各项间默认以空格作为分隔符
 print('welcome ' + username)  # 注意引号内最后的空格
+
+试着写个聊天
+
+'''
+
+print('欢迎来到输入输出基本练习，请输入您的姓名.....')
+username = input('username: ')
+print('您叫'+ username + "对吗？")
